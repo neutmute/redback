08134bcdbe7a43cc2046cdca6b44bf32c1741198
neutmute <neutmute@users.noreply.github.com>
2014-11-02 15:46:37 +1100

    chain test

--- src/Redback.Tests/SpiderWebFixture.cs
+++ src/Redback.Tests/SpiderWebFixture.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Redback.Tests.TestClasses;
+using UnitTests.TestClasses;
 
 namespace Redback.Tests
 {
@@ -17,14 +18,29 @@ namespace Redback.Tests
             var data = new MonthArray();
             var resultCount = 0;
 
-            sut.Traverse(data, delegate(TraverseResult result)
+            sut.Traverse(data, result =>
             {
                 if (resultCount == 1)
                 {
-                    Assert.That(result.Target.Value is List<int>);    
+                    Assert.That(result.Target.Value is List<int>);
                 }
                 resultCount++;
             });
+        }
+
+        /// <summary>
+        /// Not sure why it seems to recruse back out...needs investigation
+        /// </summary>
+        [Test]
+        public void Chain()
+        {
+            var data = ParentChain.GetGrandFatherSample(); 
+            var sut = new SpiderWeb();
+            var sb = new StringBuilder();
+            sut.Traverse(data, result => sb.AppendFormat("{0}|", result.Target.Value));
+
+            Console.WriteLine(sb.ToString());
+            Assert.AreEqual("greatGrandfather|grandfather|father|son|||son|son|father|father|grandfather|grandfather|greatGrandfather|", sb.ToString());
         }
     }
 }
---
+++ src/Redback.Tests/TestClasses/ParentChain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.TestClasses
+{
+    public class ParentChain
+    {
+        #region Properties
+        public ParentChain Child { get; set; }
+
+        public string Name { get; set; }
+        #endregion
+
+        #region Constructors
+        public ParentChain(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        #endregion
+
+        #region Static Methods
+        public static ParentChain GetGrandFatherSample()
+        {
+            var greatGrandfather = new ParentChain("greatGrandfather");
+            var grandfather = new ParentChain("grandfather");
+            var father = new ParentChain("father");
+            var son = new ParentChain("son");
+            
+            greatGrandfather.Child = grandfather;
+            grandfather.Child = father;
+            father.Child = son;
+
+            return greatGrandfather;
+        }
+        #endregion
+    }
+}
