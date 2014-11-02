4d32295d9a154f88b27eb6fc49806f2c93cb8a9e
neutmute <neutmute@users.noreply.github.com>
2014-11-02 15:40:10 +1100

    first test

---
+++ src/Redback.Tests/SpiderWebFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Redback.Tests.TestClasses;
+
+namespace Redback.Tests
+{
+    public class SpiderWebFixture : Fixture
+    {
+        [Test]
+        public void List()
+        {
+            var sut = new SpiderWeb();
+            var data = new MonthArray();
+            var resultCount = 0;
+
+            sut.Traverse(data, delegate(TraverseResult result)
+            {
+                if (resultCount == 1)
+                {
+                    Assert.That(result.Target.Value is List<int>);    
+                }
+                resultCount++;
+            });
+        }
+    }
+}
